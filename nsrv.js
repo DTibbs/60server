c43ea25f25ef5fc94d2bcb58631bc42b8371de53
DTibbs <david.michael.tibbetts@gmail.com>
2013-09-05 23:50:57 -0500

    Now with file uploading

--- nsrv.js
+++ nsrv.js
@@ -2,6 +2,7 @@ var util = require("util");
 var winston = require('winston');
 var express = require('express');
 var app = express();
+var fileupload = require('fileupload').createFileUpload('./imgs').middleware;
 
 //app.get('/', function(req, res){
 //	res.send('Thanks!');
@@ -16,7 +17,13 @@ app.post('/sixty:', function(req, res){
 	console.log('got post of ' + req.body.comment);
 	winston.log('info', req.body.comment);
 	res.send('Thanks!');
-})
+});
+
+app.post('/img', fileupload, function(req, res) {
+	// files are now in the req.body object
+	console.log('got file ' + util.inspect(req.body.upload));
+	res.send('Thanks!');
+});
 
 var portNum = 6060
 app.listen(portNum);
